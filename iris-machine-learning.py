e9e708555ea14862eccf63c9e2286f42db7228a7
Tristan Behrens <tristanbehrens@Nyarlathotep.local>
2018-02-12 16:25:07 +0100

    Progress.

--- iris-machine-learning.py
+++ iris-machine-learning.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import mglearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
@@ -41,11 +40,13 @@ def analyze():
         x_train,
         columns=iris_dataset.feature_names
     )
+
     pd.plotting.scatter_matrix(
         iris_dataframe, c=y_train,
         figsize=(15,15), marker="o",
-        hist_kwds={"bins": 20}, s=60, alpha=0.8, cmap=mglearn.cm3)
+        hist_kwds={"bins": 20}, s=60, alpha=0.8)
     plt.show()
+    #plt.savefig("scattermatrix.png")
 
     print("")
 
